7069fcbc12e45f26c359669651d24d25089d9f29
bruno-minimac <wbrunovieira77@gmail.com>
2024-08-02 12:30:48 -0300

    Início de projeto com as tecnologias: react native, node js

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,28 +1,32 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from '@routes/index';
+import { Routes } from "@routes/index";
 
-import { AuthContextProvider } from '@contexts/AuthContext';
+import { AuthContextProvider } from "@contexts/AuthContext";
 
-import { THEME } from './src/theme';
+import { THEME } from "./src/theme";
 
-import { Loading } from '@components/Loading';
+import { Loading } from "@components/Loading";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  return (
-    <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <AuthContextProvider>
-        {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContextProvider>
-    </NativeBaseProvider>
-  );
+    return (
+        <NativeBaseProvider theme={THEME}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <AuthContextProvider>
+                {fontsLoaded ? <Routes /> : <Loading />}
+            </AuthContextProvider>
+        </NativeBaseProvider>
+    );
 }
